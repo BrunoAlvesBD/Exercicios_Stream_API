bb1a3d8526ea1ae265cf3f448d99ef832217b90e
BrunoAlvesBD <bruno.dona@al.infnet.edu.br>
2024-07-19 11:43:41 -0300

    atualizando_02

--- src/Main/java/Desafio01/Exer01.java
+++ src/Main/java/Desafio01/Exer01.java
@@ -11,7 +11,7 @@ public class Exer01 {
     public static void main(String[] args) {
         //recebendo a lista de numeros
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
-        //agora com o stream.sorted ele já coloca os valores em ordem crescente.
+        //agora com o stream.sorted ele já coloca os valores em ordem crescente
         //com o ForEach com o Consumer para imprimir os valores.
         numeros.stream().sorted().forEach(System.out::println);
     }
